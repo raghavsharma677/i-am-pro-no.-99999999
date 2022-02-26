1e40f298ae2d78ed7384083d4f0631ed4d3c8315
raghavsharma677 <81424060+raghavsharma677@users.noreply.github.com>
2022-02-26 19:16:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent('canvas');
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
+
+
+
